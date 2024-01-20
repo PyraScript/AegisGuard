1288c7738f18b6442bd783f09b8551fdc720174d
PyraScript <157172778+PyraScript@users.noreply.github.com>
2024-01-20 18:18:10 +0330

    Update AegisGuard.sh

--- AegisGuard.sh
+++ AegisGuard.sh
@@ -10,13 +10,11 @@ sudo apt install -y ufw
 # Create a custom configuration file for SSH
 sudo cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
 
-# Path to the jail.local file
-jail_local_path="/etc/fail2ban/jail.local"
+# Remove the default [sshd] configuration
+sudo sed -i '/^\[sshd\]/,/^$/d' /etc/fail2ban/jail.local
 
-# Check if [sshd] section already exists in jail.local
-if ! grep -q "\[sshd\]" "$jail_local_path"; then
-    # [sshd] section doesn't exist, add the entire section
-    sudo tee -a "$jail_local_path" > /dev/null <<EOL
+# Add your custom [sshd] configuration
+sudo tee -a /etc/fail2ban/jail.local > /dev/null <<EOL
 [sshd]
 enabled = true
 port = ssh
@@ -25,10 +23,6 @@ logpath = /var/log/auth.log
 maxretry = 2
 bantime = 86400
 EOL
-else
-    # [sshd] section already exists, add missing options
-    sudo sed -i '/\[sshd\]/,/^\[/ s/\(port = .*\)/\1\nmaxretry = 2\nbantime = 86400/' "$jail_local_path"
-fi
 
 # Add Fail2Ban UFW action
 sudo tee -a /etc/fail2ban/action.d/ufw.conf > /dev/null <<EOL
@@ -56,5 +50,4 @@ echo "Fail2Ban installed and configured. IP banning after 2 failed SSH login att
 sudo wget -O /usr/local/bin/manage_security https://raw.githubusercontent.com/PyraScript/AegisGuard/main/manage_security
 sudo chmod +x /usr/local/bin/manage_security
 
-echo "manage_security installed. You can now use 'manage_security' command to manage Fail2Ban and UFW."
-
+echo "manage_security.sh installed. You can now use 'manage_security' command to manage Fail2Ban and UFW."
